76d0842c64946685666c1243d430c14d54997c37
宜萱 林 <108111115@mail.aeust.edu.tw>
2021-10-16 15:45:25 +0800

    新增Reflect

---
+++ 110-1Practice2-3/Reflect.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1Practice2_3
+{
+    public partial class Reflect : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //要判斷的數值
+            int M = 663166;
+
+            //辨識第幾位數(餘數)
+            int den = 0;
+
+            //計算後的餘數
+            int sum = 0;
+
+            while (M != 0)
+            {
+                sum = M % 10;
+                den = den * 10 + sum;
+                M = M / 10;
+            }
+                
+            if (M == den)
+                Response.Write("True" );
+
+            else
+                Response.Write("False" );
+        }
+    }
+}
